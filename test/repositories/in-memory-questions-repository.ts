18d1d09ef086ac293ddbe072d52d5fa0b45ebc20
Pedro Henrique <pedro.henrike2812@gmail.com>
2025-01-20 23:04:09 -0300

    feat: add  get question by slug use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,7 +1,15 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { QuestionRepository } from "@/domain/forum/application/repositories/question-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionRepository {
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) return null
+
+    return question
+  }
   public items: Question[] = []
 
   async create(question: Question): Promise<void> {
